af882bed7d72e3a2b5ff0792a4cea9a4b15929a6
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-08-03 15:18:08 +0500

    Update form-handler.js

--- js/form-handler.js
+++ js/form-handler.js
@@ -11,7 +11,7 @@ document.getElementById('staffForm').addEventListener('submit', async (e) => {
   };
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/YOUR_SUBMISSION_SCRIPT_ID/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbzdIqFQXa4_LCg87qk4njzOmUTAiE-2k7j-xdG5hRB1Vc40W7sMaoo54mCypqgcyE3I/exec', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: { 'Content-Type': 'application/json' }
